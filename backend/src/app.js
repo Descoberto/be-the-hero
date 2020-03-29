01b4d39d450978faf296014b7310a75e1316a095
Descoberto <elicodesco@gmail.com>
2020-03-29 13:19:05 -0300

    Backend updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-
-app.listen(3333);
-
 /** 
  * Rota / Recurso
  *
@@ -41,3 +29,17 @@ app.listen(3333);
  * Driver: SELECT * FROM users
  * Query Builder: table('users').select('*').where()
  */ 
+
+const express = require('express');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+module.exports = app;
