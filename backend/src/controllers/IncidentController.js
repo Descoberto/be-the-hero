869c5beef3e3cc708141d680a6dcdff16de825b5
Descoberto <50559090+Descoberto@users.noreply.github.com>
2021-04-07 23:34:03 -0300

    Update IncidentController.js

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,11 +1,11 @@
 const connection = require('../database/connection');
 
 module.exports = {
+  
   async index(request, response) {
+    
     const { page = 1 } = request.query;
-
     const [count] = await connection('incidents').count();
-
     const incidents = await connection('incidents')
     .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
     .limit(5)
@@ -25,9 +25,9 @@ module.exports = {
   },
 
   async create(request, response) {
+    
     const { title, description, value } = request.body;
     const ong_id = request.headers.authorization;
-
     const [id] = await connection('incidents').insert({
       title,
       description,
@@ -39,15 +39,16 @@ module.exports = {
   },
 
   async delete(request, response) {
+    
     const { id } = request.params;
     const ong_id = request.headers.authorization;
-
     const incident = await connection('incidents')
       .where('id', id)
       .select('ong_id')
       .first();
 
     if (incident.ong_id != ong_id) {
+      
       return response.status(401).json({error: 'Operation not permitted.'});
     } 
 
